7f236e90bad398b1f38b26f2ee7565ec4df6ee47
MaxKo1210 <115151570+MaxKo1210@users.noreply.github.com>
2023-02-05 23:51:35 +0100

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,32 +1,14 @@
-matrix =  []
+matrix =   [[9,1,0,0,3,8,0,7,0],
+            [0,0,0,6,0,0,0,0,8],
+            [0,3,0,1,0,0,0,0,0],
+            [0,0,0,8,0,0,0,0,0],
+            [0,0,2,0,0,0,0,9,0],
+            [0,6,0,0,5,4,0,0,7],
+            [0,0,0,0,1,0,0,0,0],
+            [6,0,0,0,0,0,5,0,0],
+            [0,5,0,0,7,3,0,0,4]]
 row = 9
 column = 9
-
-in_range = True
-
-print("Welcome to the Sudoku Solver")
-print("Please enter numbers between 1 and 9. If the field is empty put in a 0")
-
-#creating matrix
-def create_matrix(matrix):
-    while in_range:
-        for i in range(row):    
-            a = []
-            print("Enter the numbers for row {}".format(i+1))   
-    
-            for j in range(column):
-            
-                num = int(input())
-                if(num < 0 or num > 9):
-                    in_range = False
-                    print("Error.Please enter a number between 0 and 9")
-                    num = int(input())
-                else:
-                    in_range = True
-                    a.append(num)
-                in_range = False        
-            matrix.append(a)
-    return matrix        
 
 def print_matrix(matrix):
     for r in range(row):
@@ -39,23 +21,27 @@ def print_matrix(matrix):
                 print(matrix[r][c],end = "  ")    
         print()   
 
+#find the next empty space
 def find_empty(matrix): 
     for row in range(9): 
         for column in range(9): 
             if matrix[row][column] == 0: 
                 return row, column 
-    return 0,0 
+    return None,None
 
+#solving sudoku with recursion
 def solve(matrix): 
-    matrix = create_matrix(matrix)
     print(print_matrix(matrix))
     row, column = find_empty(matrix)
-    if row == 0 : 
-        if column == 0: 
+    #check if sudoku is completed
+    if row is None : 
+        if column is None: 
             return True
 
     for num in range(1,10):
-        if(is_valid(matrix, num, row,)):
+        print(is_valid(matrix, num, row, column))
+        
+        if is_valid(matrix, num, row, column):
             matrix[row][column] = num   
 
             if solve(matrix):
@@ -63,46 +49,56 @@ def solve(matrix):
         matrix[row][column] = 0
     return False                  
 
+#finds missing numbers in square
 def missing_square(row,column,matrix):
     missing_square= [1,2,3,4,5,6,7,8,9]
+
     for i in range(row//3*3, row//3*3+3):
         for j in range(column//3*3,column//3*3+3):
             for k in range(1,10):
                 if matrix[i][j] == k:
-                    missing_square.remove(k)
+                    if k in missing_square:
+                        missing_square.remove(k)
                     break
     return missing_square   
 
+#finds missing numbers in row
 def missing_row(row,column,matrix):
     missing_row = [1,2,3,4,5,6,7,8,9]
-    for j in range(column, column +9):
+    for j in range(0, 9):
         for k in range(1,10):
             if matrix[row][j] == 0:
                 break
             elif matrix[row][j] == k:
-                missing_row.remove(k)
+                if k in missing_row:
+                    missing_row.remove(k)
                 break
     return missing_row
 
+#finds missing numbers in column
 def missing_column(row,column,matrix):
     missing_column = [1,2,3,4,5,6,7,8,9] 
-    for i in range(row, row +9):
+    for i in range(0, 9):
         for j in range(1,10):
             if matrix[i][column] == 0:
                 break
             elif matrix[i][column] == j:
-                missing_column.remove(j)
+                if j in missing_column:
+                    missing_column.remove(j)
                 break
     return missing_column 
 
+#checks if number fits into empty space
 def is_valid(matrix,num,row,column):
-    if num in missing_square(row, column, matrix):
+    if num not in missing_square(row, column, matrix):
         return False
-    elif num in missing_row(row, column, matrix):
+    elif num not in missing_row(row, column, matrix):
         return False
-    elif num in missing_column(row, column, matrix):
+    elif num not in missing_column(row, column, matrix):
         return False        
-    else:
-         return True    
+    
+    return True    
+
 
 print(solve(matrix))
+  
